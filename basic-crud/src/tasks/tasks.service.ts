64866f797285f4088f8abf106d28a67875a6eef5
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-02-02 22:00:11 +0100

    Implemented task creation logic in TasksService with in-memory storage

--- basic-crud/src/tasks/tasks.service.ts
+++ basic-crud/src/tasks/tasks.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = []; // Para almacenar las tareas temporalmente
 
-  create(createTaskDto: CreateTaskDto) {
-    return 'This action adds a new task';
+  create(createTaskDto: CreateTaskDto): Task {
+    const newTask: Task = {
+      id: this.tasks.length + 1, //Asignamos un ID único
+      title: createTaskDto.title,
+      description: createTaskDto.description,
+      completed: false, // Las tareas empiezan como "no completadas"
+    };
+    this.tasks.push(newTask); // Agregar la nueva tarea al array
+    return newTask; // retornar la tarea creada
   }
 
   findAll() {
