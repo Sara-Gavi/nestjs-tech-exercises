81dea4896ff316db56f2593cbbc29ba82616c67f
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-02-20 19:57:41 +0100

    Generated Cats resource and installed TypeORM with MySQL support

--- mysql-crud/src/app.module.ts
+++ mysql-crud/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { CatsModule } from './cats/cats.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }), // Habilitar variables de entorno
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306, // puerto correcto de MySQL en Workbench
+      username: 'user_crud', // usuario de MySQL
+      password: 'root',
+      database: 'db_crud',
+      autoLoadEntities: true, // Carga las entidades automáticamente
+      synchronize: true, // Crea y actualiza tablas automáticamente NO USAR EN PRODUCCION)
+    }),
+    CatsModule, // Módulo de ejemplo del tutorial
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
