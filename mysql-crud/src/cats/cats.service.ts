b6eae8303abfc46f5a8f38da0720a7932784d900
Sara-Gavi <sara.gavilan.castro@gmail.com>
2025-02-21 17:33:22 +0100

    code from cats.service.ts file to talk to the database and do operations

--- mysql-crud/src/cats/cats.service.ts
+++ mysql-crud/src/cats/cats.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { Cat } from './entities/cat.entity';
 
 @Injectable()
 export class CatsService {
+  constructor(
+    @InjectRepository(Cat)
+    private catsRepository: Repository<Cat>,
+  ) {}
+
   create(createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
   findAll() {
-    return `This action returns all cats`;
+    return this.catsRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} cat`;
+    return this.catsRepository.findOneBy({ id });
   }
 
   update(id: number, updateCatDto: UpdateCatDto) {
@@ -21,6 +29,6 @@ export class CatsService {
   }
 
   remove(id: number) {
-    return `This action removes a #${id} cat`;
+    return this.catsRepository.delete(id);
   }
 }
